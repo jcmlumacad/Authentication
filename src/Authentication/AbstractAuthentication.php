9509cfe0dac977f5253f7bb37715a874f7c23978
Daryl Eisner <ucsdmath.project@gmail.com>
2016-10-24 12:47:08 -0700

    Update

--- src/Authentication/AbstractAuthentication.php
+++ src/Authentication/AbstractAuthentication.php
@@ -63,7 +63,7 @@ abstract class AbstractAuthentication implements AuthenticationInterface, Servic
      *
      * @api
      */
-    const VERSION = '1.10.0';
+    const VERSION = '1.11.0';
 
     //--------------------------------------------------------------------------
 
