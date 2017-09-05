24f977a58471d4b8f33af634477fb8f57a969f4f
UCSDMath <ucsdmath.project@gmail.com>
2017-09-05 16:23:16 -0700

    Update

--- src/Authentication/Authentication.php
+++ src/Authentication/Authentication.php
@@ -39,7 +39,7 @@ class Authentication extends AbstractAuthentication implements AuthenticationInt
      *
      * @api
      */
-    public const VERSION = '1.25.0';
+    public const VERSION = '1.26.0';
 
     //--------------------------------------------------------------------------
 
